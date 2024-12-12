4d6f7d2d97265f8c67ce96fb37335961629c2c81
Celstren <31768104+Celstren@users.noreply.github.com>
2024-12-11 21:54:14 -0500

    Splash view update

--- lib/views/splash_view.dart
+++ lib/views/splash_view.dart
@@ -1,11 +1,6 @@
-import 'package:lottie/lottie.dart';
-import 'package:medito/repositories/auth/auth_repository.dart';
 import 'package:medito/services/notifications/firebase_notifications_service.dart';
-import 'package:medito/utils/stats_manager.dart';
-import 'package:medito/views/bottom_navigation/bottom_navigation_bar_view.dart';
 import 'package:medito/views/downloads/downloads_view.dart';
 import 'package:medito/views/onboarding/onboarding_view.dart';
-import 'package:medito/views/root/root_page_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:medito/utils/fade_page_route.dart';
@@ -18,22 +13,17 @@ class SplashView extends ConsumerStatefulWidget {
 }
 
 class _SplashViewState extends ConsumerState<SplashView> with TickerProviderStateMixin {
-  late final AnimationController _controller;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+    _initializeUser();
+    _initializeFirebaseMessaging();
   }
 
   void _initializeUser() async {
     try {
+      await Future.delayed(const Duration(seconds: 3));
       await Navigator.of(context).pushReplacement(
         FadePageRoute(
           builder: (context) => const OnboardingView(),
@@ -56,24 +46,19 @@ class _SplashViewState extends ConsumerState<SplashView> with TickerProviderStat
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      resizeToAvoidBottomInset: false,
-      body: SizedBox(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: Lottie.asset(
-          'assets/animations/Splash_Animation.json',
-          controller: _controller,
-          fit: BoxFit.fill,
-          onLoaded: (composition) {
-            _controller
-              ..duration = const Duration(milliseconds: 1000)
-              ..forward().whenComplete(() {
-                _initializeUser();
-                _initializeFirebaseMessaging();
-              });
-          },
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        resizeToAvoidBottomInset: false,
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Image.asset(
+            'assets/images/splash_screen_background.png',
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
