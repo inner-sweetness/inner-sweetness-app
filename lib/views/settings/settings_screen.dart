c55a533517def89d74e5f377c3a011c579834e1c
Celstren <31768104+Celstren@users.noreply.github.com>
2024-12-15 17:14:35 -0500

    Logout

--- lib/views/settings/settings_screen.dart
+++ lib/views/settings/settings_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
 import 'package:medito/constants/constants.dart';
+import 'package:medito/injection.dart';
 import 'package:medito/providers/providers.dart';
 import 'package:medito/utils/fade_page_route.dart';
 import 'package:medito/views/edit_account/edit_account_screen.dart';
+import 'package:medito/views/login/bloc/login_bloc.dart';
+import 'package:medito/views/login/login_view.dart';
 import 'package:medito/views/submit_feedback/submit_feedback_screen.dart';
 import 'package:medito/views/unsubscribe/unsubscribe_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -24,11 +27,11 @@ TimeOfDay? _getReminderTimeFromPrefs(SharedPreferences prefs) {
       : null;
 }
 
-class SettingsScreen extends ConsumerWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final List<SettingsItem> settingsItems = [
       SettingsItem(
         type: 'url',
@@ -54,151 +57,172 @@ class SettingsScreen extends ConsumerWidget {
         ),
       ),
     ];
-
-    return Scaffold(
-      backgroundColor: const Color(0xFF1E1E1E),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              GestureDetector(
-                onTap: Navigator.of(context).maybePop,
-                behavior: HitTestBehavior.opaque,
-                child: const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Icon(
-                    Icons.arrow_back,
-                    size: 24,
-                    color: Colors.white,
+    final loginBloc = getIt<LoginBloc>();
+    return BlocListener<LoginBloc, LoginState>(
+      bloc: loginBloc,
+      listener: (context, state) async {
+        if (state is LogoutSuccessState) {
+          var canPop = Navigator.canPop(context);
+          while(canPop) {
+            await Navigator.maybePop(context);
+            canPop = Navigator.canPop(context);
+          }
+          Navigator.of(context).pushReplacement(
+            FadePageRoute(
+              builder: (context) => const LoginView(),
+            ),
+          );
+        }
+      },
+      child: Scaffold(
+        backgroundColor: const Color(0xFF1E1E1E),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                GestureDetector(
+                  onTap: Navigator.of(context).maybePop,
+                  behavior: HitTestBehavior.opaque,
+                  child: const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Icon(
+                      Icons.arrow_back,
+                      size: 24,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              Container(
-                height: 132,
-                width: 132,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(color: const Color(0x33FFFFFF), width: 4),
-                  boxShadow: const <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(0, 2),
-                      blurRadius: 2,
-                      spreadRadius: 2,
-                    ),
-                  ],
-                ),
-                child: Center(
-                  child: ClipOval(
-                    child: SizedBox(
-                      height: 124,
-                      width: 124,
-                      child: Image.asset(
-                        'assets/images/profile_picture.jpeg',
-                        fit: BoxFit.fill,
+                const SizedBox(height: 32),
+                Container(
+                  height: 132,
+                  width: 132,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: const Color(0x33FFFFFF), width: 4),
+                    boxShadow: const <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.black26,
+                        offset: Offset(0, 2),
+                        blurRadius: 2,
+                        spreadRadius: 2,
+                      ),
+                    ],
+                  ),
+                  child: Center(
+                    child: ClipOval(
+                      child: SizedBox(
+                        height: 124,
+                        width: 124,
+                        child: Image.asset(
+                          'assets/images/profile_picture.jpeg',
+                          fit: BoxFit.fill,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                'Aaron Márquez',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'aaron@email.com',
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w400,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 16),
-              Center(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: const Color(0xFF0150FF),
-                  ),
-                  child: const Text(
-                    'Black Plan',
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                'MORE',
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w700,
-                ),
-                textAlign: TextAlign.start,
-              ),
-              const SizedBox(height: 16),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: settingsItems.map((item) => _buildMenuItemTile(context, ref, item)).toList(),
-              ),
-              const SizedBox(height: 32),
-              GestureDetector(
-                onTap: () => Navigator.of(context).push(
-                  FadePageRoute(
-                    builder: (context) => const UnsubscribeScreen(),
-                  ),
-                ),
-                behavior: HitTestBehavior.opaque,
-                child: Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: const Color(0xFFFF9900)),
-                    borderRadius: BorderRadius.circular(56),
-                  ),
-                  child: const Text(
-                    'UNSUBSCRIBE',
-                    style: TextStyle(
-                      color: Color(0xFFFF9900),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-              const Padding(
-                padding: EdgeInsets.all(16),
-                child: Text(
-                  'DELETE ACCOUNT',
+                const SizedBox(height: 32),
+                const Text(
+                  'Aaron Márquez',
                   style: TextStyle(
+                    fontSize: 24,
                     color: Colors.white,
-                    fontSize: 16,
                     fontWeight: FontWeight.w700,
                   ),
                   textAlign: TextAlign.center,
                 ),
-              ),
-            ],
+                const SizedBox(height: 8),
+                const Text(
+                  'aaron@email.com',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 16),
+                Center(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: const Color(0xFF0150FF),
+                    ),
+                    child: const Text(
+                      'Black Plan',
+                      style: TextStyle(
+                        fontSize: 14,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 32),
+                const Text(
+                  'MORE',
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.start,
+                ),
+                const SizedBox(height: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: settingsItems.map((item) => _buildMenuItemTile(context, item)).toList(),
+                ),
+                const SizedBox(height: 32),
+                GestureDetector(
+                  onTap: () => Navigator.of(context).push(
+                    FadePageRoute(
+                      builder: (context) => const UnsubscribeScreen(),
+                    ),
+                  ),
+                  behavior: HitTestBehavior.opaque,
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: const Color(0xFFFF9900)),
+                      borderRadius: BorderRadius.circular(56),
+                    ),
+                    child: const Text(
+                      'UNSUBSCRIBE',
+                      style: TextStyle(
+                        color: Color(0xFFFF9900),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 24),
+                GestureDetector(
+                  onTap: () => loginBloc.add(const Logout()),
+                  behavior: HitTestBehavior.opaque,
+                  child: const Padding(
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      'LOG OUT',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -206,10 +230,9 @@ class SettingsScreen extends ConsumerWidget {
   }
 
   Widget _buildMenuItemTile(
-    BuildContext context,
-    WidgetRef ref,
-    SettingsItem item,
-  ) {
+      BuildContext context,
+      SettingsItem item,
+      ) {
     return GestureDetector(
       onTap: item.onTap,
       behavior: HitTestBehavior.opaque,
