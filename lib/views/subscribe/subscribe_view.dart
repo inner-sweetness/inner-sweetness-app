59117c758fae92c06c13dee555f5da79a9c46cbc
Celstren <31768104+Celstren@users.noreply.github.com>
2024-12-21 08:50:33 -0500

    Subscription view

--- lib/views/subscribe/subscribe_view.dart
+++ lib/views/subscribe/subscribe_view.dart
@@ -26,7 +26,7 @@ class _SubscribeViewState extends ConsumerState<SubscribeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF03F480),
+      backgroundColor: Colors.white,
       body: SafeArea(
         child: Column(
           mainAxisSize: MainAxisSize.max,
@@ -40,129 +40,121 @@ class _SubscribeViewState extends ConsumerState<SubscribeView> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Padding(
-                        padding: const EdgeInsets.only(right: 24),
-                        child: GestureDetector(
-                          onTap: initializeUser,
-                          behavior: HitTestBehavior.opaque,
-                          child: const Icon(
-                            Icons.close,
-                            color: Colors.white,
-                            size: 32,
+                    Material(
+                      color: const Color(0xFFFEC107),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 24, top: 18, bottom: 16),
+                              child: GestureDetector(
+                                onTap: initializeUser,
+                                behavior: HitTestBehavior.opaque,
+                                child: const Icon(
+                                  Icons.close,
+                                  color: Colors.black,
+                                  size: 24,
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
+                          Image.asset(
+                            'assets/images/subscribe_background.png',
+                            fit: BoxFit.contain,
+                            height: 280,
+                          ),
+                        ],
                       ),
                     ),
                     const SizedBox(height: 36),
                     const Text(
-                      'If it resonates',
+                      'Watch new content',
                       style: TextStyle(
-                        fontWeight: FontWeight.w400,
-                        fontSize: 16,
-                        color: Color(0xFF0150FF),
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    const Text(
-                      'Subscribe!',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 40,
+                        fontWeight: FontWeight.w800,
+                        fontSize: 24,
                         color: Colors.black,
+                        letterSpacing: -1.25
                       ),
                       textAlign: TextAlign.center,
                     ),
-                    const SizedBox(height: 36),
-                    const Row(
+                    const SizedBox(height: 24),
+                    const Text(
+                      'You will get access on our biweekly content. Explore podcast and articles as you want on your phone.',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                          color: Color(0xFF020202),
+                          letterSpacing: -.75
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 24),
+                    Row(
                       mainAxisSize: MainAxisSize.max,
                       mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
-                        Row(
-                          mainAxisSize: MainAxisSize.min,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              '\$',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 32,
-                                color: Color(0xFF0150FF),
-                                height: 2,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                            Text(
-                              '5.55',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 64,
-                                color: Color(0xFF0150FF),
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
-                        ),
-                        Text(
-                          '/month',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w700,
-                            fontSize: 16,
-                            color: Color(0xFF0150FF),
-                            height: 3.5,
+                        Container(
+                          height: 6,
+                          width: 6,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFFF9900),
                           ),
-                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(width: 8),
+                        Container(
+                          height: 6,
+                          width: 6,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFC6C7C6),
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Container(
+                          height: 6,
+                          width: 6,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFC6C7C6),
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Container(
+                          height: 6,
+                          width: 6,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFFC6C7C6),
+                          ),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 36),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 39),
-                      child: RichText(
-                        textAlign: TextAlign.center,
-                        text: const TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                              text: 'And you will receive our fresh hand crafted content',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w400,
-                                color: Colors.black,
-                                fontSize: 16,
-                                overflow: TextOverflow.clip,
-                              ),
-                            ),
-                            TextSpan(
-                              text: ' every two weeks.',
-                              style: TextStyle(
-                                fontWeight: FontWeight.w700,
-                                color: Colors.black,
-                                fontSize: 16,
-                                overflow: TextOverflow.clip,
-                              ),
-                            ),
-                          ],
-                        ),
+                    const SizedBox(height: 48),
+                    const Text(
+                      'Starting at \$10.00/month',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w200,
+                          fontSize: 16,
+                          color: Color(0xFF6A6C6A),
+                          letterSpacing: .25
                       ),
-                    ),
-                    const SizedBox(height: 36),
-                    Center(
-                      child: Image.asset(
-                        'assets/images/subscription_image.png',
-                        fit: BoxFit.cover,
-                        width: 255,
-                      ),
+                      textAlign: TextAlign.center,
                     ),
                   ],
                 ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+              padding: const EdgeInsets.all(18),
               child: AppButton(
-                text: 'PAY NOW!',
+                text: 'CONTINUE TO CHECKOUT',
+                color: const Color(0xFF0150FF),
                 radius: 40,
                 onTap: initializeUser,
               ),
